5cceb4b1332bb60fc23f5efba5553fe0780d1450
Felix Ingram <felix.ingram@nccgroup.com>
2022-02-06 23:32:44 +0000

    Added a skeleton for a cli

---
+++ src/dark_star/__main__.py
@@ -0,0 +1,21 @@
+import click
+
+
+@click.group("cli")
+@click.pass_context
+def cli(ctx):
+    """Dark Star - a web framework based on Starlette"""
+    pass
+
+
+@cli.command("create-app")
+def create_app():
+    print("Creating app")
+
+
+@cli.command("new-route")
+def new_route():
+    print("Adding new route")
+
+
+cli(prog_name="cli")
